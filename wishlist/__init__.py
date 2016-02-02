4b84afee09713286d1e340ec68b2ce35c571a5b7
Darin Douglass <ddouglass@barracuda.com>
2016-02-02 13:14:27 -0500

    Initial commit of project structure

---
+++ wishlist/__init__.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""An interface to Steam, Amazon, and other wishlist services."""
+
+from wishlist import metadata
+
+
+__version__ = metadata.version
+__author__ = metadata.authors[0]
+__license__ = metadata.license
+__copyright__ = metadata.copyright
---
+++ wishlist/metadata.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+"""Project metadata
+
+Information describing the project.
+"""
+
+# The package name, which is also the "UNIX name" for the project.
+package = 'wishlist'
+project = 'Wishlist'
+project_no_spaces = project.replace(' ', '')
+version = '0.1'
+description = "An interface to Steam, Amazon, and other wishlist services."
+authors = ['Darin Douglass', 'Nathan Hatfield']
+authors_string = ', '.join(authors)
+emails = ['douglassdarin@gmail.com', 'nathanhatfield@me.com']
+license = 'MIT'
+copyright = '2016 ' + authors_string
+url = 'http://example.com/'
